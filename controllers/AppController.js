de0501df7e7d9272032cc1bdac33bee6f8b46155
KagisoMakgaba1 <kagisomakgaba99@gmail.com>
2024-10-01 03:03:33 -0700

    all

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,44 +1,16 @@
-/**
- * AppController class representing the controller for the application.
- */
-import dbClient from '../utils/db';
-import HTTPError from '../utils/httpErrors';
 import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-/**
- * AppController class to handle application status and statistics.
- */
 class AppController {
-  /**
-   * Get the status of Redis and the database.
-   * @param {Object} req - The request object.
-   * @param {Object} res - The response object.
-   * @returns {Object} JSON response with the status of Redis and the database.
-   */
-  static async getStatus(req, res) {
-    try {
-      return res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
-    } catch (error) {
-      return HTTPError.internalServerError(res);
-    }
+  static getStatus(request, response) {
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
-  /**
-   * Get the number of users and files in the database.
-   * @param {Object} req - The request object.
-   * @param {Object} res - The response object.
-   * @returns {Promise<Object>} JSON response with the number of users and files.
-   */
-  static async getStats(req, res) {
-    try {
-      return res
-        .status(200)
-        .json({ users: await dbClient.nbUsers(), files: await dbClient.nbFiles() });
-    } catch (error) {
-      console.log(`Error occurring here: ${error.message}`);
-      return HTTPError.internalServerError(res);
-    }
+  static async getStats(request, response) {
+    const usersNum = await dbClient.nbUsers();
+    const filesNum = await dbClient.nbFiles();
+    response.status(200).json({ users: usersNum, files: filesNum });
   }
 }
 
-export default AppController;
+module.exports = AppController;
